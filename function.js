57d12adc91adfee72c170f9aebca4d1e28c2142a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-30 01:52:16 +0600

    perametter.js

--- function.js
+++ function.js
@@ -1,1 +1,15 @@
+function startFun() {
+  console.log("stand up");
+  console.log("start the switch");
+}
 
+startFun();
+
+// perameter functions
+
+function bringSingaar(money) {
+  console.log("Mama singara den");
+  console.log(money);
+}
+
+bringSingaar(30);
---
+++ perametter.js
@@ -0,0 +1,7 @@
+function sum(a, b, c, d) {
+  console.log(a, b, c, d);
+  var sum = a + b + c + d;
+  console.log(sum);
+}
+
+sum(14, 36, 10, 23);
